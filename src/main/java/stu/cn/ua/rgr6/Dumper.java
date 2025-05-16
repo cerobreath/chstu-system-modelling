f9da34c2334ec98fe3aab64ae541beb5b412d305
Dylan <cerobreath@gmail.com>
2025-05-16 22:54:18 +0300

    feat: add experimental regression analysis functionality
    - Defined a controllable factor for experimentation
    - Added a new "Regres" tab to the UI with the ExperimentManager component
    - Configured ChooseData component in ExperimentManager to allow factor value selection
    - Implemented IExperimentable interface in Model class with initForExperiment() and getResultOfExperiment() methods
    - Enabled regression mode testing to generate system parameter dependency graphs

---
+++ src/main/java/stu/cn/ua/rgr6/Dumper.java
@@ -0,0 +1,81 @@
+package stu.cn.ua.rgr6;
+
+import process.Actor;
+import process.DispatcherFinishException;
+import process.QueueForTransactions;
+import process.Store;
+import rnd.Randomable;
+import stat.Histo;
+
+import java.util.function.BooleanSupplier;
+
+public class Dumper extends Actor {
+    private QueueForTransactions<Dumper> queueToExcavator;
+    private QueueForTransactions<Dumper> queueToLoader;
+    private Store bunker;
+    private double finishTime;
+    private int bodySize;
+    private Randomable rnd;
+    private int bodyLoad;
+    private Histo histoServiceTime;
+    private double serviceStartTime;
+
+    @Override
+    protected void rule() throws DispatcherFinishException {
+        BooleanSupplier isBodyFull = () -> isFull();
+        while (getDispatcher().getCurrentTime() <= finishTime) {
+            serviceStartTime = getDispatcher().getCurrentTime();
+            holdForTime(rnd.next());
+            queueToExcavator.addLast(this);
+            queueToLoader.addLast(this);
+            waitForCondition(isBodyFull, "кузов має бути повним");
+            getDispatcher().printToProtocol(
+                    getNameForProtocol() + " їде до бункера");
+            holdForTime(rnd.next());
+            getDispatcher().printToProtocol(
+                    getNameForProtocol() + " розвантажується");
+            bunker.add(bodyLoad);
+            double serviceTime = getDispatcher().getCurrentTime() - serviceStartTime;
+            histoServiceTime.add(serviceTime);
+            bodyLoad = 0;
+        }
+    }
+
+    public void addPortion() {
+        bodyLoad++;
+        getDispatcher().printToProtocol(
+                getNameForProtocol() + "- у кузові стало " + bodyLoad);
+    }
+
+    public boolean isFull() {
+        return bodyLoad >= bodySize;
+    }
+
+    public void setQueueToExcavator(QueueForTransactions<Dumper> queueToExcavator) {
+        this.queueToExcavator = queueToExcavator;
+    }
+
+    public void setQueueToLoader(QueueForTransactions<Dumper> queueToLoader) {
+        this.queueToLoader = queueToLoader;
+    }
+
+    public void setBunker(Store bunker) {
+        this.bunker = bunker;
+    }
+
+    public void setFinishTime(double finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public void setBodySize(int bodySize) {
+        this.bodySize = bodySize;
+    }
+
+    public void setRnd(Randomable rnd) {
+        this.rnd = rnd;
+    }
+
+    public void setHistoForServiceTime(Histo histo) {
+        this.histoServiceTime = histo;
+    }
+}
---
+++ src/main/java/stu/cn/ua/rgr6/Loader.java
@@ -0,0 +1,52 @@
+package stu.cn.ua.rgr6;
+
+import process.Actor;
+import process.DispatcherFinishException;
+import process.QueueForTransactions;
+import process.Store;
+import rnd.Randomable;
+import java.util.function.BooleanSupplier;
+
+public class Loader extends Actor {
+    private Store oreHeap;
+    private QueueForTransactions<Dumper> queue;
+    private double finishTime;
+    private Randomable rnd;
+
+    @Override
+    protected void rule() throws DispatcherFinishException {
+        BooleanSupplier isDumper = () -> queue.size() > 0;
+        BooleanSupplier heapSize = () -> oreHeap.getSize() > 0;
+        while (getDispatcher().getCurrentTime() <= finishTime) {
+            waitForCondition(isDumper, "має бути самоскид");
+            Dumper dumper = queue.removeFirst();
+            while (!dumper.isFull()) {
+                waitForCondition(heapSize, "має бути руда в купі");
+                getDispatcher().printToProtocol(
+                        getNameForProtocol() + " бере порцію руди");
+                oreHeap.remove(1);
+                holdForTime(rnd.next());
+                dumper.addPortion();
+                getDispatcher().printToProtocol(
+                        getNameForProtocol() + " додає порцію руди до самоскида " +
+                                dumper.getNameForProtocol());
+            }
+        }
+    }
+
+    public void setOreHeap(Store oreHeap) {
+        this.oreHeap = oreHeap;
+    }
+
+    public void setQueue(QueueForTransactions<Dumper> queue) {
+        this.queue = queue;
+    }
+
+    public void setFinishTime(double finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public void setRnd(Randomable rnd) {
+        this.rnd = rnd;
+    }
+}
