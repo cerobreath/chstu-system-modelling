d8bda00e5b050c63cfd22582462e5473bfef4f87
Dylan <cerobreath@gmail.com>
2025-05-16 20:41:04 +0300

    feat: add statistics collection and visualization to simulation model
    - Implemented statistics collection for queue load, actor wait time, and total transaction service time
    - Integrated existing histograms with appropriate components
    - Implemented IStatisticsable interface in Model class with methods initForStatistics() and getStatistics()
    - Added StatisticsManager as a new tab (Stat) in the Main JTabbedPane
    - Configured StatisticsManager to create model instances using the factory
    - Tuned model parameters to prevent empty or infinitely growing queues
    - Set simulation time to 10000 time units for meaningful statistical output

---
+++ src/main/java/stu/cn/ua/rgr5/Crusher.java
@@ -0,0 +1,39 @@
+package stu.cn.ua.rgr5;
+
+import process.Actor;
+import process.DispatcherFinishException;
+import process.Store;
+import rnd.Randomable;
+import java.util.function.BooleanSupplier;
+
+public class Crusher extends Actor {
+    private Store bunker;
+    private double finishTime;
+    private Randomable rnd;
+
+    @Override
+    protected void rule() throws DispatcherFinishException {
+        BooleanSupplier bunkerHasOre = () -> bunker.getSize() > 0;
+        while (getDispatcher().getCurrentTime() <= finishTime) {
+            waitForCondition(bunkerHasOre, "має бути руда в бункері");
+            getDispatcher().printToProtocol(
+                    getNameForProtocol() + " бере порцію руди");
+            bunker.remove(1);
+            holdForTime(rnd.next());
+            getDispatcher().printToProtocol(
+                    getNameForProtocol() + " завершив обробку порції");
+        }
+    }
+
+    public void setBunker(Store bunker) {
+        this.bunker = bunker;
+    }
+
+    public void setFinishTime(double finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public void setRnd(Randomable rnd) {
+        this.rnd = rnd;
+    }
+}
---
+++ src/main/java/stu/cn/ua/rgr5/Model.java
@@ -0,0 +1,192 @@
+package stu.cn.ua.rgr5;
+
+import process.Dispatcher;
+import process.MultiActor;
+import process.QueueForTransactions;
+import process.Store;
+import stat.DiscretHisto;
+import stat.Histo;
+import stat.IHisto;
+import widgets.stat.IStatisticsable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Model implements IStatisticsable {
+    private Dispatcher dispatcher;
+    private Main gui;
+
+    private Excavator excavator;
+    private Loader loader;
+    private Dumper dumper;
+    private MultiActor multiDumper;
+    private Crusher crusher;
+
+    private Store oreHeap;
+    private Store bunker;
+
+    private QueueForTransactions<Dumper> queueToExcavator;
+    private QueueForTransactions<Dumper> queueToLoader;
+
+    DiscretHisto histoQueueToExcavator = new DiscretHisto();
+    DiscretHisto histoQueueToLoader = new DiscretHisto();
+    Histo histoOreHeap = new Histo();
+    Histo histoBunker = new Histo();
+    Histo histoExcavatorWait = new Histo();
+    Histo histoLoaderWait = new Histo();
+    Histo histoCrusherWait = new Histo();
+    Histo histoDumperWait = new Histo();
+    Histo histoDumperServiceTime = new Histo();
+
+    public Model(Dispatcher d, Main g) {
+        if (d == null || g == null) {
+            System.out.println("Не визначено диспетчера або GUI для Model");
+            System.out.println("Подальша робота неможлива");
+            System.exit(0);
+        }
+        dispatcher = d;
+        gui = g;
+        componentsToStartList();
+    }
+
+    private void componentsToStartList() {
+        dispatcher.addStartingActor(getExcavator());
+        dispatcher.addStartingActor(getLoader());
+        dispatcher.addStartingActor(getMultiDumper());
+        dispatcher.addStartingActor(getCrusher());
+    }
+
+    public void initForTest() {
+        getOreHeap().setPainter(gui.getDiagramOrePileSize().getPainter());
+        getQueueToExcavator().setPainter(gui.getDiagramQueueDumpTrucksToExcavator().getPainter());
+        getQueueToLoader().setPainter(gui.getDiagramQueueToLoader().getPainter());
+        getBunker().setPainter(gui.getDiagramCrusherCondition().getPainter());
+
+        if (gui.getConsoleLoggerCheckBox().isSelected()) {
+            dispatcher.setProtocolFileName("Console");
+        } else {
+            dispatcher.setProtocolFileName("");
+        }
+    }
+
+    public Excavator getExcavator() {
+        if (excavator == null) {
+            excavator = new Excavator();
+            excavator.setNameForProtocol("Excavator");
+            excavator.setHistoForActorWaitingTime(histoExcavatorWait);
+            excavator.setOreHeap(getOreHeap());
+            excavator.setQueue(getQueueToExcavator());
+            excavator.setOreHeapMaxSize(gui.getChooseDataOrePileSize().getDouble());
+            excavator.setRnd(gui.getChooseRandomExcavator().getRandom());
+            excavator.setFinishTime(gui.getChooseDataSimulationTime().getDouble());
+        }
+        return excavator;
+    }
+
+    public Loader getLoader() {
+        if (loader == null) {
+            loader = new Loader();
+            loader.setNameForProtocol("Loader");
+            loader.setHistoForActorWaitingTime(histoLoaderWait);
+            loader.setOreHeap(getOreHeap());
+            loader.setQueue(getQueueToLoader());
+            loader.setRnd(gui.getChooseRandomLoader().getRandom());
+            loader.setFinishTime(gui.getChooseDataSimulationTime().getDouble());
+        }
+        return loader;
+    }
+
+    public Dumper getDumper() {
+        if (dumper == null) {
+            dumper = new Dumper();
+            dumper.setNameForProtocol("Dumper");
+            dumper.setHistoForActorWaitingTime(histoDumperWait);
+            dumper.setHistoForServiceTime(histoDumperServiceTime);
+            dumper.setQueueToExcavator(getQueueToExcavator());
+            dumper.setQueueToLoader(getQueueToLoader());
+            dumper.setBunker(getBunker());
+            dumper.setBodySize(gui.getChooseDataPortionOreDumpTruck().getInt());
+            dumper.setRnd(gui.getChooseRandomLoader().getRandom());
+            dumper.setFinishTime(gui.getChooseDataSimulationTime().getDouble());
+        }
+        return dumper;
+    }
+
+    public MultiActor getMultiDumper() {
+        if (multiDumper == null) {
+            multiDumper = new MultiActor();
+            multiDumper.setNameForProtocol("MultiActor для самоскидів");
+            multiDumper.setOriginal(getDumper());
+            multiDumper.setNumberOfClones(gui.getChooseDataDumper().getInt());
+        }
+        return multiDumper;
+    }
+
+    public Crusher getCrusher() {
+        if (crusher == null) {
+            crusher = new Crusher();
+            crusher.setNameForProtocol("Crusher");
+            crusher.setHistoForActorWaitingTime(histoCrusherWait);
+            crusher.setBunker(getBunker());
+            crusher.setRnd(gui.getChooseRandomCrusher().getRandom());
+            crusher.setFinishTime(gui.getChooseDataSimulationTime().getDouble());
+        }
+        return crusher;
+    }
+
+    public Store getOreHeap() {
+        if (oreHeap == null) {
+            oreHeap = new Store();
+            oreHeap.setNameForProtocol("Ore Heap");
+            oreHeap.setDispatcher(dispatcher);
+            oreHeap.setHisto(histoOreHeap);
+        }
+        return oreHeap;
+    }
+
+    public Store getBunker() {
+        if (bunker == null) {
+            bunker = new Store();
+            bunker.setNameForProtocol("Bunker");
+            bunker.setDispatcher(dispatcher);
+            bunker.setHisto(histoBunker);
+        }
+        return bunker;
+    }
+
+    public QueueForTransactions<Dumper> getQueueToExcavator() {
+        if (queueToExcavator == null) {
+            queueToExcavator = new QueueForTransactions<>("Queue to Excavator", dispatcher, histoQueueToExcavator);
+            queueToExcavator.setNameForProtocol("Queue to Excavator");
+        }
+        return queueToExcavator;
+    }
+
+    public QueueForTransactions<Dumper> getQueueToLoader() {
+        if (queueToLoader == null) {
+            queueToLoader = new QueueForTransactions<>("Queue to Loader", dispatcher, histoQueueToLoader);
+            queueToLoader.setNameForProtocol("Queue to Loader");
+        }
+        return queueToLoader;
+    }
+
+    @Override
+    public void initForStatistics() {
+        // Порожній, оскільки ініціалізація не потрібна
+    }
+
+    @Override
+    public Map<String, IHisto> getStatistics() {
+        Map<String, IHisto> map = new HashMap<>();
+        map.put("Черга до екскаватора", histoQueueToExcavator);
+        map.put("Черга до завантажувача", histoQueueToLoader);
+        map.put("Розмір купи руди", histoOreHeap);
+        map.put("Розмір бункера", histoBunker);
+        map.put("Час очікування екскаватора", histoExcavatorWait);
+        map.put("Час очікування завантажувача", histoLoaderWait);
+        map.put("Час очікування дробарки", histoCrusherWait);
+        map.put("Час очікування самоскидів", histoDumperWait);
+        map.put("Час обслуговування самоскидів", histoDumperServiceTime);
+        return map;
+    }
+}
